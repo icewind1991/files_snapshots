cdae87cb269b5a8007bb8925877e3b4e475732c6
Robin Appelman <robin@icewind.nl>
2017-05-29 13:06:58 +0200

    settings css
    Signed-off-by: Robin Appelman <robin@icewind.nl>

--- templates/settings.php
+++ templates/settings.php
@@ -1,5 +1,6 @@
 <?php
 script('files_snapshots', 'settings');
+style('files_snapshots', 'settings');
 
 /** @var \OCP\IL10N $l */
 /** @var array $_ */
@@ -7,16 +8,28 @@ script('files_snapshots', 'settings');
 <form id="files_snapshots" class="section" action="#" method="post">
 	<h2><?php p($l->t('Snapshots')); ?></h2>
 
-	<p>
-		<label for="format">Snapshot format</label>
-		<input id="format" value="<?php p($_['snapshot_format']) ?>"
-			   placeholder="/path/to/snapshots/%snapshot%/data"/>
-	</p>
-	<p>
-		<label for="date_format">Date format</label>
-		<input id="date_format" value="<?php p($_['date_format']) ?>"/>
-	</p>
-	<p>
-		<input type="submit" value="Save"/>
-	</p>
+	<table class="settings">
+		<tr>
+			<td>
+				<label for="format">Snapshot format</label>
+			</td>
+			<td>
+				<input id="format" value="<?php p($_['snapshot_format']) ?>"
+					   placeholder="/path/to/snapshots/%snapshot%/data"/>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<label for="date_format">Date format</label>
+			</td>
+			<td>
+				<input id="date_format" value="<?php p($_['date_format']) ?>"/>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<input type="submit" value="Save"/>
+			</td>
+		</tr>
+	</table>
 </form>
